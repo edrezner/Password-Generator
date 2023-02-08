9eb28d5522e21cbd7f3c43065948efd31686a833
Seven <edrezner7@gmail.com>
2023-02-07 22:25:58 -0500

    setup main generatePassword function with initial alert box; moving onto password length next

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
+  function generatePassword() {
+    // found info for alert, prompt and confirm message boxes here: https://www.tutorialsteacher.com/javascript/display-popup-message-in-javascript#:~:text=alert(message)%3A%20Display%20a,the%20OK%20and%20Cancel%20buttons.
+    alert("The upcoming prompts will set a password length and then the password criteria you wish to use.");
+    
+    var passwordLength = 
+    prompt()
+}
+  
   passwordText.value = password;
 
 }
