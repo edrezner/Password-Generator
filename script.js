93a548043faeebf0d095f9d7ef2d9206dd1a08a9
Seven <edrezner7@gmail.com>
2023-02-08 16:46:00 -0500

    added alert msg requiring at least one criteria to be chosen; added confirm variables for criteria

--- script.js
+++ script.js
@@ -10,16 +10,22 @@ function writePassword() {
     // found info for alert, prompt and confirm message boxes here: https://www.tutorialsteacher.com/javascript/display-popup-message-in-javascript#:~:text=alert(message)%3A%20Display%20a,the%20OK%20and%20Cancel%20buttons.
     alert("The upcoming prompts will set a password length and then the password criteria you wish to use.");
     
-    var passwordLength = prompt("Choose the length of your password. Your password must be 8 to 128 characters.");
+    var pwLength = prompt("Choose the length of your password. Your password must be 8 to 128 characters.");
 
     // mdn web doc page on while loops https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration#while_statement
-    while (passwordLength < 8 || passwordLength > 128) {
+    while (pwLength < 8 || pwLength > 128) {
       prompt("Please choose a password length from 8 to 128 characters.");
     }
 
+    alert("Your password must contain at least one of the 4 following criteria:")
     
+    var pwLower = confirm("Would you like your password to include lowercase letters?");
+    var pwUpper = confirm("Would you like your password to include upper case letters?");
+    var pwNumber = confirm("Would you like your password to inlcude numbers?")    
+    var pwSpecial = confirm("Would you like your password to inlcude special characters?");
 
-}
+
+  }
   
   passwordText.value = password;
 
